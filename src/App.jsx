7068dc80463be7cb346fa55ad54b3d4fa2bae151
Alek <ehrgein@gmail.com>
2022-12-17 15:52:08 -0300

    Data scraped successfully

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Data from './components/Data'
+import bajatension from '../index.js'
+
+
+
+function App() {
+
+console.log(bajatension)
+
+
+  return (
+    <div className="App">
+      <Data/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Data.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <div className='w-[100vw] h-[100vh] flex justify-center items-center'>
+
+    </div>
+  )
+}
+
+export default Data
